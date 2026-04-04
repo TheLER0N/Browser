522015104cbf513d077d621ec7322c4fb67a17d8
LERON <164945360+TheLER0N@users.noreply.github.com>
2026-04-04 16:26:28 +0300

    Удаление мусорных файлов: publish папка, исправление .gitignore

--- publish_standalone/diagnose.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 > nul
-echo Диагностика запуска GhostBrowser...
-echo.
-set COMPlus_ThrowUnobservedThreadExceptions=1
-set DOTNET_EnableWriteXorExecute=0
-set DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=0
-GhostBrowser.exe
-pause
